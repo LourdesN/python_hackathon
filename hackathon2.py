44fbb4cc9d8b351c71519b940eb981aabaf5afd1
Lourdes <121977047+LourdesN@users.noreply.github.com>
2024-03-16 12:13:40 +0300

    Add files via upload

---
+++ hackathon2.py
@@ -0,0 +1,8 @@
+age = int(input("Please enter your age: "))
+print(f"your age is :{age}")
+
+if age <18:
+    print("You are not eligible to vote!!")
+
+else:
+     print("You are eligible to vote!!")
